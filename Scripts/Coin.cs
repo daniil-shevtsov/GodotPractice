d03134be4f8fcc018ef1f7c5c6b565017d004166
Daniil Shevtsov <daniil.shevtsov@aviasales.ru>
2023-11-18 20:03:21 +0400

    almost migrated coin to C#

--- Scripts/Coin.cs
+++ Scripts/Coin.cs
@@ -1,20 +1,34 @@
 using Godot;
 using System;
+using static System.MathF;
 
-public partial class Coin : Node
+public partial class Coin : Area2D
 {
-    int timePassed = 0;
+    [Export]
+    int amplitude = 4;
+
+    [Export]
+    int frequency = 5;
+
+    double timePassed = 0.0;
     Vector2 initialPosition = Vector2.Zero;
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        GD.Print("C# Coin Ready");
+        initialPosition = Position;
     }
 
     // Called every frame. 'delta' is the elapsed time since the previous frame.
     public override void _Process(double delta)
     {
-        GD.Print("C# Coin Process");
+        coinHover(delta);
+    }
+
+    public void coinHover(double delta)
+    {
+        timePassed += delta;
+        var newY = initialPosition.Y + amplitude * Sin((float)(frequency * timePassed));
+        Position = new Vector2(Position.X, newY);
     }
 }
