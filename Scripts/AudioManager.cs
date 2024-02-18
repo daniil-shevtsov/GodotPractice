b7df30a49737f9b5466462bb3b4b5d8dffcb968f
Daniil Shevtsov <daniil.shevtsov@aviasales.ru>
2024-02-18 12:24:12 +0400

    before changing computer

--- Scripts/AudioManager.cs
+++ Scripts/AudioManager.cs
@@ -1,10 +1,15 @@
 using Godot;
 using System;
 
-public partial class new_script : Node
+public partial class AudioManager : Node
 {
+    AudioEffect jumpSfx = null;
+
     // Called when the node enters the scene tree for the first time.
-    public override void _Ready() { }
+    public override void _Ready()
+    {
+        jumpSfx = GetNode<AudioEffect>("JumpSfx");
+    }
 
     // Called every frame. 'delta' is the elapsed time since the previous frame.
     public override void _Process(double delta) { }
